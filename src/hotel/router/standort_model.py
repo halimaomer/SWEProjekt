0976c3fad260fdbcaabb9eaf94cafdd5b62bca82
Alisa Knoebl <knal1011@h-ka.de>
2026-04-02 16:34:23 +0200

    standort_model und zimmer_model hinzugefügt

--- src/hotel/router/standort_model.py
+++ src/hotel/router/standort_model.py
@@ -4,5 +4,46 @@ from typing import Annotated
 
 from pydantic import BaseModel, ConfigDict, StringConstraints
 
+from hotel.entity import Standort
+
+__all__: list[str] = ["StandortModel"]
 
 
+class StandortModel(BaseModel):
+    """Pydantic-Model für den Standort."""
+
+    strasse: Annotated[str, StringConstraints(max_length=64)]
+    """Straße"""
+    hausnummer: Annotated[str, StringConstraints(max_length=10)]
+    """Hausnummer"""
+    plz: Annotated[str, StringConstraints(pattern=r"^\d{5}$")]
+    """Postleitzahl"""
+    ort: Annotated[str, StringConstraints(max_length=64)]
+    """Ort"""
+    land: Annotated[str, StringConstraints(max_length=64)]
+    """Land"""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "strasse": "Muster Str.",
+                "hausnummer": "1a",
+                "plz": "12345",
+                "ort": "Musterstadt",
+                "land": "Deutschland"
+            }
+        }
+    )
+
+    def to_standort(self) -> Standort:
+        """Konvertierung in ein Standort-Objekt für SQLAlchemy.
+
+        :return: Standort-Objekt für SQLAlchemy
+        :rtype: Standort
+        """
+        standort_dict = self.model_dump()
+        standort_dict["id"] = None
+        standort_dict["hotel_id"] = None
+        standort_dict["hotel"] = None
+
+        return Standort(**standort_dict)
---
+++ src/hotel/router/zimmer_model.py
@@ -0,0 +1,41 @@
+"""Pydantic-Model für das Zimmer."""
+
+from decimal import Decimal
+from typing import Annotated
+
+from pydantic import BaseModel, ConfigDict, StringConstraints
+
+from hotel.entity import Zimmer
+
+__all__ = ["ZimmerModel"]
+
+
+class ZimmerModel(BaseModel):
+    """Pydantic-Model für das Zimmer."""
+
+    preis: Decimal
+    """Der Preis."""
+    zimmernummer: Annotated[str, StringConstraints(max_length=10)]
+    """Die Zimmernummer."""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "preis": "100",
+                "zimmernummer": "1"
+            }
+        }
+    )
+
+    def to_zimmer(self) -> Zimmer:
+        """Konvertierung in ein Zimmer-Objekt für SQLAlchemy.
+
+        :return: Zimmer-Objekt für SQLAlchemy
+        :rtype: Zimmer
+        """
+        zimmer_dict = self.model_dump()
+        zimmer_dict["id"] = None
+        zimmer_dict["hotel_id"] = None
+        zimmer_dict["hotel"] = None
+
+        return Zimmer(**zimmer_dict)
